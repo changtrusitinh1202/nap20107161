44c040f85b1915f4e5716a8174fadf71ccb51f2c
iuh20107161 <86941258+changtrusitinh1202@users.noreply.github.com>
2024-03-28 15:22:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello! Student code is 20107161);
+  res.send('Hello! Student code is 20107161');
 });
 
 app.listen(PORT, HOST);
